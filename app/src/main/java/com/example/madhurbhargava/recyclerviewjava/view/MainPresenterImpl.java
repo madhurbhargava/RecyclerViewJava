67cb5eb89a6552dc2e71b1323c58c1c3acb73fba
Madhur Bhargava <madhur.bhargava333@gmail.com>
2018-06-13 08:06:35 +0200

    Added List configuration

--- app/src/main/java/com/example/madhurbhargava/recyclerviewjava/view/MainPresenterImpl.java
+++ app/src/main/java/com/example/madhurbhargava/recyclerviewjava/view/MainPresenterImpl.java
@@ -2,24 +2,38 @@ package com.example.madhurbhargava.recyclerviewjava.view;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.paging.ItemKeyedDataSource;
+import android.arch.paging.LivePagedListBuilder;
 import android.arch.paging.PagedList;
 import android.view.View;
 
 import com.example.madhurbhargava.recyclerviewjava.model.Cryptocurrency;
 import com.example.madhurbhargava.recyclerviewjava.model.DataError;
 import com.example.madhurbhargava.recyclerviewjava.network.CryptoRepository;
+import com.example.madhurbhargava.recyclerviewjava.paging.CryptocurrencyDataSourceFactory;
 import com.example.madhurbhargava.recyclerviewjava.paging.ItemKeyedCryptoDataSource;
 
 import java.util.List;
+import java.util.concurrent.Executor;
 
 public class MainPresenterImpl implements MainPresenter, MainPresenter.DataUpdater {
 
     public LiveData<PagedList<Cryptocurrency>> userList;
     private MainView view;
     LiveData<ItemKeyedCryptoDataSource> dataSource;
+    Executor executor;
 
     public MainPresenterImpl(MainView view) {
         this.view = view;
+        PagedList.Config pagedListConfig =
+                (new PagedList.Config.Builder()).setEnablePlaceholders(false)
+                        .setInitialLoadSizeHint(10)
+                        .setPageSize(20).build();
+
+        CryptocurrencyDataSourceFactory dataSourceFactory = new CryptocurrencyDataSourceFactory();
+
+        userList = (new LivePagedListBuilder(dataSourceFactory, pagedListConfig))
+                .build();
+
     }
 
     @Override
