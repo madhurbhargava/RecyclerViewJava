a1d1d296bff8cda8931aaef65ff4492afcdbc2e2
Madhur Bhargava <madhur.bhargava333@gmail.com>
2018-06-26 23:48:23 +0200

    Removed MainPresenter

--- app/src/main/java/com/example/madhurbhargava/recyclerviewjava/view/MainPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.example.madhurbhargava.recyclerviewjava.view;
-
-import com.example.madhurbhargava.recyclerviewjava.network.CryptoRepository;
-import com.example.madhurbhargava.recyclerviewjava.model.DataError;
-import com.example.madhurbhargava.recyclerviewjava.model.Cryptocurrency;
-
-import java.util.List;
-
-
